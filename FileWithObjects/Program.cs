8ce9df5c9e85f8fa5662f364f7e6e4d049fcd3fd
Samarth Goel <samarth.goel2@mindtree.com>
2022-05-07 12:42:40 +0530

    File with objects

---
+++ FileWithObjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWithObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Samarth";
+            person.Age = 22;
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("D:\\Input.txt", FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, person);
+            stream.Close();
+
+            stream = new FileStream("D:\\Input.txt", FileMode.Open, FileAccess.Read);
+
+            Person p = (Person)formatter.Deserialize(stream);
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Age);
+
+            Console.ReadKey();
+        }
+    }
+}
